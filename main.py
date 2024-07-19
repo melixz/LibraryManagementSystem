12caea6bdeae5a8112a2319a16256cc548dd39fb
Vlad <dr.melix@gmail.com>
2024-07-19 15:20:47 +0300

    Добавил класс Book и добавил метод to_dict для сериализации

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    pass
 
 
-if __name__ == "__main__":
-    main()
+class Book:
+    def __init__(self, id, title, author, year, status="available"):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.year = year
+        self.status = status
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "year": self.year,
+            "status": self.status
+        }
